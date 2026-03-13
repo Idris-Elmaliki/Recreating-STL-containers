7a72d0ae5830c36f0b71809a022e5a539c435515
Idris <idris.elmaliki06@gmail.com>
2026-03-13 18:08:22 -0400

    Created push_front() and pop_front()

--- Containers/deque.cpp
+++ Containers/deque.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
+#include <deque>
 
 class deque {
     static const size_t BLOCK_SIZE = 8;
 
-    class Block { 
-    public:
+    struct Block { 
         int* m_arr;  
         size_t currentAmount; // always set to 0, NOT 1
 
@@ -19,29 +19,30 @@ class deque {
             return this->currentAmount; 
         }
 
-        Block operator++(int) {
+        size_t operator++(int) {
             Block temp; 
             temp.currentAmount = this->currentAmount; 
-            ++(this->currentAmount); 
+            this->currentAmount += 1; 
             
-            return temp; 
+            return temp.currentAmount; 
         }
 
-        Block& operator--() {
+        size_t& operator--() {
             currentAmount -= 1; 
-            return *this;
+            return this->currentAmount;
         }
 
-        Block operator--(int) {
+        size_t operator--(int) {
             Block temp; 
             temp.currentAmount = this->currentAmount; 
-            --(*this);
+            this->currentAmount -= 1;
 
-            return temp; 
+            return temp.currentAmount; 
         }
 
         ~Block() {
             delete[] m_arr; 
+            std::cout << "Deleted Block Arr\n";
         }
     }; 
 
@@ -49,12 +50,13 @@ class deque {
 
     size_t m_map_capacity; // current size of alloc memory
     size_t m_index_size; // all indexes that have values
+    size_t m_first_block_offset; 
 
     size_t m_map_start; 
     size_t m_map_end;
     
     // allocs the m_map @ the constructor
-    inline void AllocateMapArray(const size_t& mapSize) {
+    inline void AllocateMapArray(const size_t& mapSize = 1) {
         m_map = new Block*[mapSize]; 
 
         this->m_map_capacity = mapSize; 
@@ -62,39 +64,62 @@ class deque {
     }
 
     // copy alloc for m_map 
+    // Problem for pop_front lies here!
     inline void ReallocateMapArray() {
-        Block** newMap = new Block*[ReallocMapSize()]; 
-        
-        size_t left = 0;
-        size_t right = m_map_capacity - 1; 
-        size_t offset = m_map_capacity / 2; 
+        std::cout << "In ReallocateMapArray()\n"; 
 
-        while(left != offset && right != (m_map_capacity - offset)) {
+        int newMapCapacity = 0;
+
+        if(m_map_capacity == 1) {
+            newMapCapacity = 3;
+        }
+        else {
+            newMapCapacity = ReallocMapSize();
+        }
+
+        Block** newMap = new Block*[newMapCapacity]; 
+
+        std::cout << "m_map_capacity: " << m_map_capacity << '\n'; 
+
+        size_t left = 0;
+        size_t right = newMapCapacity - 1; 
+        size_t offset = left + (right - left / 2) - 1; 
+
+        std::cout << "left: " << left << '\n';
+        std::cout << "right: " << right << '\n';
+
+        while(left < offset && right > (newMapCapacity - offset) - 1) {
+            std::cout << "Entered the loop!\n";
             newMap[left] = nullptr; 
             newMap[right] = nullptr; 
 
             ++left, --right; 
+
+            std::cout << "left: " << left << '\n';
+            std::cout << "right: " << right << '\n';
         }
 
-        for(size_t i = 0; i < m_map_capacity; i++) 
+        for(size_t i = 0; i < m_map_capacity; i++) {
             newMap[offset + i] = m_map[i];  
-        
+        }
         delete[] m_map; 
 
-        m_map_start = left; 
+        m_map_start = offset - left;  
         m_map_end = offset + m_map_capacity - 1; 
+        m_map_capacity = newMapCapacity;
 
         m_map = newMap; 
-        m_map_capacity = ReallocMapSize(); 
 
-        for(size_t i = 0; i < m_map_capacity; i++) {
+        for(size_t i = 0; i < newMapCapacity; i++) {
             if(newMap[i] == nullptr) {
-                std::cout << "NULL" << '\n'; 
+                std::cout << "nullptr" << '\n'; 
             }
             else {
                 std::cout << i << '\n'; 
             }
         }
+
+        std::cout << "m_map_start: " << m_map_start << '\n'; 
     }
     
     // allocs a new block
@@ -108,9 +133,6 @@ class deque {
 
         Block* newBlock = new Block; 
 
-        // this for loop condition checks two things:
-            // 1.) whether
-            // 2.) 
         for(size_t i = 0; i < indexRange || i < BLOCK_SIZE; i++) {
             newBlock->m_arr[i] = newData; 
 
@@ -127,25 +149,32 @@ class deque {
         return (indexSize/BLOCK_SIZE) + 1; 
     }
 
-    size_t ReallocMapSize() {
+    inline constexpr size_t getBlockAmount(const size_t &indexSize) const {
+        if(indexSize < BLOCK_SIZE - 1)
+            return indexSize; 
+
+        return BLOCK_SIZE - 1; 
+    }
+ 
+    inline size_t ReallocMapSize() {
         return m_map_capacity * 2; 
     }
 
-    size_t ReallocMapSizeDifference() {
+    inline size_t ReallocMapSizeDifference() {
         return m_map_capacity / 2; 
     }
     
 public: 
     // default constructor
     deque()
-    : m_map_capacity(1), m_index_size(0), m_map_start(0)
+    : m_index_size(0), m_map_start(0), m_first_block_offset(0)
     {
-        AllocateMapArray(1); 
+        AllocateMapArray(); 
     }
 
     // deque(indexSize, arrData)
     deque(const size_t& indexSize, const int& arrData = 0) 
-    : m_index_size(indexSize), m_map_start(0)
+    : m_index_size(indexSize), m_map_start(0), m_first_block_offset(0)
     {
         AllocateMapArray(getMapSize(indexSize));  
         
@@ -161,14 +190,14 @@ public:
 
     // deque = {1, 2, 3, ..., n}
     deque(std::initializer_list<int> list) 
-    : m_index_size(list.size()), m_map_start(0)
+    : m_index_size(list.size()), m_map_start(0), m_first_block_offset(0) 
     {
         AllocateMapArray(getMapSize(list.size())); 
 
         size_t currentMapIndex = 0; 
         Block* newBlock = AllocateNewBlock(); 
 
-        for(auto data : list) {
+        for(const auto& data : list) {
             if(newBlock->currentAmount == BLOCK_SIZE) {
                 m_map[currentMapIndex] = newBlock; 
                 newBlock = AllocateNewBlock(); 
@@ -194,16 +223,18 @@ public:
 
     int back() {
         // we get the last block, and the last available index in the block
-        return m_map[m_map_end]->m_arr[m_map[m_map_end]->currentAmount - 1];
+        return m_map[m_map_end]->m_arr[m_map[m_map_end]->currentAmount];
     }
 
     void push_back(const int& newData) {
-        // something wrong in push_back!!!
+        std::cout << "m_index_size: " << m_index_size << "\n";
+        std::cout << "m_map_start: " << m_map_start << '\n'; 
+        std::cout << "m_map_end: " << m_map_end << '\n';
 
         if(m_index_size == 0) {
             m_map[m_map_start]->m_arr[0] = newData; 
         }
-        else if(m_map[m_map_end]->currentAmount == BLOCK_SIZE) {
+        else if(m_map[m_map_end]->currentAmount == BLOCK_SIZE - 1) {
             std::cout << "Need to realloc\n"; 
 
             ReallocateMapArray(); 
@@ -216,13 +247,79 @@ public:
             std::cout << "New Data: " << m_map[m_map_end]->m_arr[i] << '\n';
         }
 
-        m_map[m_map_end]->currentAmount++; // fixed for now
+        ++*(m_map[m_map_end]);
         ++m_index_size; 
 
         std::cout << "m_map[m_map_end]->currentAmount: " << m_map[m_map_end]->currentAmount << '\n'; 
         std::cout << "m_index_size: " << m_index_size << '\n';
 
         std::cout << "push_back was sucessful!\n\n"; 
+    }
+
+    // void emplace_back(int&& newData) {}
+
+    void pop_back() {
+        if(m_index_size == 0)
+            return; 
+
+        size_t& index = (m_map[m_map_end]->currentAmount);
+
+        // m_map[m_map_end]->m_arr[index].~T(); <----- [This is for when we include templates]!
+
+        --index; 
+        --m_index_size; 
+
+        if(index == 0) {
+            delete m_map[m_map_end];
+            --m_map_end;
+        }
+    }
+
+    void push_front(const int& newData) {
+        if(m_first_block_offset != 0) {
+            size_t& index = (m_map[m_map_start]->currentAmount);
+
+            m_first_block_offset = BLOCK_SIZE - index - 1; 
+            m_map[m_map_start]->m_arr[m_first_block_offset] = newData; 
+        }
+        else {
+            std::cout << "Time to realloc!\n";
+            ReallocateMapArray();
+
+            m_map[m_map_start] = AllocateNewBlock();
+            m_first_block_offset = BLOCK_SIZE - 1; 
+
+            m_map[m_map_start]->m_arr[m_first_block_offset] = newData; 
+        }
+
+        ++m_index_size; 
+        ++*(m_map[m_map_start]);
+
+        std::cout << "m_map[m_map_start]->currentAmount: " << m_map[m_map_start]->currentAmount << '\n'; 
+        std::cout << "m_index_size: " << m_index_size << '\n';
+
+        std::cout << "push_front was sucessful!\n\n"; 
+    }
+
+    // void emplace_front(int&& newData)
+
+    void pop_front() {
+        if(m_index_size == 0)
+            return;
+
+        size_t& index = (m_map[m_map_start]->currentAmount);
+
+        // m_map[m_map_start]->m_arr[index].~T(); <----- [This is for when we include templates]!
+
+        --index; 
+        --m_index_size; 
+        ++m_first_block_offset; 
+
+        if(index == 0) {
+            delete m_map[m_map_start]; 
+            ++m_map_start; 
+            --m_map_capacity;
+        }
     }
 
     void clear() {
@@ -251,41 +348,48 @@ public:
     }
 
     int& operator[](const size_t &index) {
-        std::cout << "m_index: " << m_index_size << "\nindex: " << index << '\n';
+        if (index >= m_index_size || index < 0) 
+            std::out_of_range("deque index out of range");
 
-        size_t block = (m_index_size + index) / BLOCK_SIZE; 
-        size_t block_index = index % BLOCK_SIZE; 
 
-        std::cout << "Address of: " << std::addressof(m_map[block]->m_arr[block_index]) << "\n"; 
+        size_t abs = m_first_block_offset + index;  
+        size_t block = m_map_start + (abs / BLOCK_SIZE); 
+        size_t block_index = abs % BLOCK_SIZE; 
 
         return m_map[block]->m_arr[block_index]; 
     }
 
     constexpr int operator[](const size_t &index) const {
-        size_t block = (m_index_size + index) / BLOCK_SIZE;  
+        if (index >= m_index_size || index < 0)
+            std::out_of_range("deque index out of range");
+
+        size_t block = index / BLOCK_SIZE + m_map_start; 
         size_t block_index = index % BLOCK_SIZE; 
 
         return m_map[block]->m_arr[block_index]; 
     }
 
     ~deque() {
-        for(size_t i = 0; i < m_map_capacity; i++)  {
-            std::cout << "Deleted!\n";
+        for(size_t i = m_map_start; i < m_map_end + 1; i++)  {
             delete m_map[i]; 
+            std::cout << "Deleted Block!\n";
         }
         
         delete m_map; 
+        std::cout << "Deleted Map!\n";
     }
 }; 
 
 int main(void) {
     deque d = {1, 2, 3, 4};  
 
-    for(size_t i = 0; i < d.size(); i++) 
-        std::cout << d[i] << "\n\n";
-
-    std::cout << std::endl; 
+    d.push_front(-1); 
+    d.push_front(5); 
 
     d.push_back(9);
 
+    for(size_t i = -1; i < d.size(); i++) 
+        std::cout << d[i] << "\n\n";
+
+    std::cin.get();
 }
